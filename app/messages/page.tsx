a189ff499c771a285ed2dfd2f0e5d8e27b42852d
Triananda Fajar <mail.trianandafajar@gmail.com>
2026-01-29 13:28:36 +0700

    fix: update session retrieval in MessagesPage to include authOptions

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,12 +1,12 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import ChatApp from "./ChatApp";
+import { authOptions } from "@/lib";
+import { headers } from "next/headers";
 
 export default async function MessagesPage() {
-  const session = await getServerSession();
-
-  console.log("test", session);
-  
+  headers();
+  const session = await getServerSession(authOptions);
   if (!session) redirect("/");
 
   return <ChatApp />;
